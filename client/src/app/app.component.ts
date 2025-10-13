255ec9b667d61703f9ce156ce2ed1e7963da14ca
Edwin <edwinjebakumar.p@gmail.com>
2025-10-14 00:18:46 +0400

    API calls from Angular, Tailwind, Angular material

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { HeaderComponent } from "./layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  protected readonly title = 'Edkart';
+export class AppComponent implements OnInit {
+  baseUrl ='https://localhost:5001/api/';
+  private httpClient = inject(HttpClient);
+  title = 'Edkart';
+  products: Product[] = [];
+
+  ngOnInit(): void {
+    this.httpClient.get<Pagination<Product>>(this.baseUrl+'products').subscribe({
+      next: response => this.products = response.data,
+      error: error => console.error('There was an error!', error),
+      complete: () => console.log('Request completed')
+    });
+  }
 }
